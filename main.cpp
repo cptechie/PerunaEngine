20dcff468eba4ee349255547f89e046c4dfd1c79
cptechie <chintanpatel@usr127-134.perunanet.smu.edu>
2015-06-10 13:07:59 -0500

    Moved source files to work with folder structure change

--- main.cpp
+++ main.cpp
@@ -105,7 +105,7 @@ int main(void)
 	glEnable(GL_DEPTH_TEST);
 
 	
-	Shader* s = new Shader("/Users/33993405/Desktop/computer_graphics_class/GLFW_Demo/GLFW_Demo/simpleShader01.vert", "/Users/33993405/Desktop/computer_graphics_class/GLFW_Demo/GLFW_Demo/simpleShader01.frag");
+	Shader* s = new Shader("/Users/chintanpatel/Dropbox/School/Computer_Graphics/GitHub/PerunaEngine/simpleShader01.vert", "/Users/chintanpatel/Dropbox/School/Computer_Graphics/GitHub/PerunaEngine/simpleShader01.frag");
 
 	glm::vec4 cols[] = {
 		glm::vec4(1.0, 0.0, 0.0, 1.0),
