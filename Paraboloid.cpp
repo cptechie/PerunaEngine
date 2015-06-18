0bf218f1cca5ef525cb46049b573b2493fd86963
cptechie <chintanpatel@usr127-134.perunanet.smu.edu>
2015-06-18 13:14:25 -0500

    Semi-working Paraboloid

--- Paraboloid.cpp
+++ Paraboloid.cpp
@@ -1,16 +1,16 @@
 //
-//  Cylinder.cpp
+//  Paraboloid.cpp
 //  GLFW_Demo
 //
 //  Created by Ira on 6/11/15.
 //  Copyright (c) 2015 C3. All rights reserved.
 //
 
-#include "Cylinder.h"
+#include "Paraboloid.h"
 
-Cylinder::Cylinder():cylinderRadius(.5), cylinderHeight(1), cylinderDetail(10){
+Paraboloid::Paraboloid():paraboloidRadius(.5), paraboloidHeight(1), paraboloidVerticalDetail(10), paraboloidRingDetail(10), paraboloidCurvature(5){
     
-    int vertices = cylinderDetail * 2 + 2;
+    int vertices = paraboloidVerticalDetail * paraboloidRingDetail + 2;
     
     for(int i=0; i<vertices; ++i){
         cols.push_back(glm::vec4(.75, .25, .64, 1.0));
@@ -19,56 +19,87 @@ Cylinder::Cylinder():cylinderRadius(.5), cylinderHeight(1), cylinderDetail(10){
     init();
 }
 
-Cylinder::Cylinder(float cylinderRadius, float cylinderHeight, int cylinderDetail, const glm::vec4& col):
-cylinderRadius(cylinderRadius), cylinderHeight(cylinderHeight), cylinderDetail(cylinderDetail) {
+Paraboloid::Paraboloid(float paraboloidRadius, float paraboloidHeight, int paraboloidVerticalDetail, int paraboloidRingDetail, float paraboloidCurvature, const glm::vec4& col):
+paraboloidRadius(paraboloidRadius), paraboloidHeight(paraboloidHeight), paraboloidVerticalDetail(paraboloidVerticalDetail), paraboloidRingDetail(paraboloidRingDetail) {
     
-    int vertices = cylinderDetail * 2 + 2;
+    int vertices = paraboloidVerticalDetail * paraboloidRingDetail + 2;
     
     for(int i=0; i<vertices; ++i){
-        cols.push_back(col);
+        cols.push_back(glm::vec4(.75, .25, .64, 1.0));
     }
     
     init();
 }
 
 
-void Cylinder::calcVerts() {
-    float theta = 0.0;
+void Paraboloid::calcVerts() {
     
-    verts.push_back(glm::vec3(0, 0, cylinderHeight/2));
-    verts.push_back(glm::vec3(0, 0, -cylinderHeight/2));
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
     
-    for(int i=0; i<cylinderDetail; ++i){
+    verts.push_back(glm::vec3(x, y, z));
+    verts.push_back(glm::vec3(x, paraboloidHeight, z));
+    
+    
+    for(int i=0; i<paraboloidHeight; ++i){
+        
+        y += paraboloidHeight/paraboloidVerticalDetail;
+        
+        float radius = sqrt(y);
+        
+        float theta = 0;
+        
+        for(int j = 0; j < paraboloidRingDetail; j++){
+            
+            x = radius*cos(theta);
+            z = radius*sin(theta);
+            
+            verts.push_back(glm::vec3(x, y, z));
+            
+            int a = i*paraboloidRingDetail+j;
+            int b = i*paraboloidRingDetail+j+1;
+            int c = (i+1)*paraboloidRingDetail+j;
+            int d = (i+1)*paraboloidRingDetail+j+1;
+            
+//            // end cases
+//            int e = i *paraboloidRingDetail;
+//            int f = (i+1)*paraboloidRingDetail;
+//            int g = j+1;
+            
+            if (i<paraboloidVerticalDetail-1){
+                if (j<paraboloidVerticalDetail-1){
+                    inds.push_back(Elem(a, d, b));
+                    inds.push_back(Elem(a, c, d));
+                }
+//                else {
+//                    // close ring
+//                    inds.push_back(Elem(a, f, e));
+//                    inds.push_back(Elem(a, c, f));
+//                }
+//                // close toroid
+            }
+//            else {
+//                if (j<paraboloidVerticalDetail-1){
+//                    inds.push_back(Elem(a, g, b));
+//                    inds.push_back(Elem(a, j, g));
+//                } else {
+//                    // close ring
+//                    inds.push_back(Elem(a, 0, e));
+//                    inds.push_back(Elem(a, j, 0));
+//                }
+//                
+//                
+//            }
 
-        verts.push_back(glm::vec3(cylinderRadius*cos(theta), cylinderRadius*sin(theta), cylinderHeight/2));
-        verts.push_back(glm::vec3(cylinderRadius*cos(theta), cylinderRadius*sin(theta), -cylinderHeight/2));
-
-        theta += M_PI*2/cylinderDetail;
+            
+            
+            theta += M_PI*2/paraboloidRingDetail;
+        }
     }
 }
 
-void Cylinder::calcInds() {
+void Paraboloid::calcInds() {
 
-    for (int i = 0; i < cylinderDetail - 1; i++) {
-        
-        int a = i * 2 + 2;
-        int b = a + 2;
-        int c = i * 2 + 3;
-        int d = c + 2;
-        inds.push_back(Elem(0, a, b));
-        inds.push_back(Elem(1, c, d));
-        
-        inds.push_back(Elem(c, d, b));
-        inds.push_back(Elem(c, b, a));
-    }
-
-    int g = cylinderDetail * 2;
-    int h = cylinderDetail * 2 + 1;
-
-    inds.push_back(Elem(0, g, 2));
-    inds.push_back(Elem(1, h, 3));
-    
-    inds.push_back(Elem(2, h, 3));
-    inds.push_back(Elem(2, g, h));
 }
 
--- Paraboloid.h
+++ Paraboloid.h
@@ -1,32 +1,32 @@
 //
-//  Cylinder.h
+//  Paraboloid.h
 //  GLFW_Demo
 //
 //  Created by Ira on 6/11/15.
 //  Copyright (c) 2015 C3. All rights reserved.
 //
 
-#ifndef __GLFW_Demo__Cylinder__
-#define __GLFW_Demo__Cylinder__
+#ifndef __GLFW_Demo__Paraboloid__
+#define __GLFW_Demo__Paraboloid__
 
 #include <stdio.h>
 #include "Geom.h"
 
-class Cylinder: public Geom {
+class Paraboloid: public Geom {
     
 public:
-    Cylinder();
-    Cylinder(float cylinderRadius, float cylinderHeight, int cylinderDetail, const glm::vec4& col = glm::vec4(.76, .56, .456, 1.0));
+    Paraboloid();
+    Paraboloid(float paraboloidRadius, float paraboloidHeight, int paraboloidVerticalDetail, int paraboloidRingDetail, float paraboloidCurvature, const glm::vec4& col = glm::vec4(.76, .56, .456, 1.0));
     // must ocreated concrete implementations
     // of abstract functions (pure virtuals)
     void calcVerts();
     void calcInds();
     
     // properties
-    float cylinderRadius, cylinderHeight;
-    int cylinderDetail;
+    float paraboloidRadius, paraboloidHeight, paraboloidCurvature;
+    int paraboloidVerticalDetail, paraboloidRingDetail;
     
 };
 
 
-#endif /* defined(__GLFW_Demo__Cylinder__) */
+#endif /* defined(__GLFW_Demo__Paraboloid__) */
