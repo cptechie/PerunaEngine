3e77ecec82ba40de805e9466a9f7a0d08280f9fc
cptechie <chintanpatel@usr127-134.perunanet.smu.edu>
2015-06-24 12:52:14 -0500

    Added more assimp stuff

---
+++ Mesh.h
@@ -0,0 +1,14 @@
+//
+//  Mesh.h
+//  GLFW_Demo
+//
+//  Created by Chintan Patel on 6/24/15.
+//  Copyright (c) 2015 C3. All rights reserved.
+//
+
+#ifndef __GLFW_Demo__Mesh__
+#define __GLFW_Demo__Mesh__
+
+#include <stdio.h>
+
+#endif /* defined(__GLFW_Demo__Mesh__) */
---
+++ Model.h
@@ -0,0 +1,14 @@
+//
+//  Model.h
+//  GLFW_Demo
+//
+//  Created by Chintan Patel on 6/24/15.
+//  Copyright (c) 2015 C3. All rights reserved.
+//
+
+#ifndef __GLFW_Demo__Model__
+#define __GLFW_Demo__Model__
+
+#include <stdio.h>
+
+#endif /* defined(__GLFW_Demo__Model__) */
